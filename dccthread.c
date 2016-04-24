7e79b204ce78fad95337e728f989f18833e7383f
Leandro Vieira <lvieiraa@users.noreply.github.com>
2016-04-24 00:26:40 -0300

    criando as funcoes:
     dccthread_yield(void),  const char *dccthread_name(dccthread_t *tid),  dccthread_t * dccthread_self(void)
    
    Ainda falta o gerenciador

--- dccthread.c
+++ dccthread.c
@@ -5,6 +5,7 @@
 #include "dccthread.h"
 #include <signal.h>
 
+#define MEM 64000
 #define TAM 400 //tamanho maximo da fila de threads
 #define handle_error(msg) \
            do { perror(msg); exit(EXIT_FAILURE); } while (0) //mensagem de erro copiado do man da funcaos da biblioteca ucontext.h
@@ -15,10 +16,32 @@ int ultimo = 0;
 
 ucontext_t gerente, principal;
 
+dccthread_t * dccthread_self(void)
+{
+  // Return the running thread.
+  return fila[ultimo];
+}
+
+void dccthread_yield(void){
+
+swapcontext(&(fila[ultimo]->estado), &gerente);
+
+}
+
+void gerenciador(void) {
+printf("tem que implementar o gerenciador!\n");
+setcontext(&(fila[ultimo]->estado));
+} 
+
+const char *dccthread_name(dccthread_t *tid)
+{
+  return tid->tid;
+} 
+
 void iniciarContexto(ucontext_t *contexto)
 {
-  contexto->uc_stack.ss_sp = malloc(SIGSTKSZ); //SIGSTKSZ definido na biblioteca signal.h  tamanho do sinal 
-  contexto->uc_stack.ss_size = SIGSTKSZ;
+  contexto->uc_stack.ss_sp = malloc(MEM);
+  contexto->uc_stack.ss_size = MEM;
   contexto->uc_stack.ss_flags = 0;
 }
 
@@ -26,11 +49,18 @@ void iniciarContexto(ucontext_t *contexto)
 void dccthread_init(void (*func)(int), int param){
 	
 	
+ atual = 0;
+    ultimo = 0;
 	if (getcontext(&gerente) == -1) //inicia  a struct gerente 
 	handle_error("getcontext");
+	gerente.uc_link = &principal;
 	iniciarContexto(&gerente);//inicia a pilha, preenchendo  os campos necessarios da uc_stack 
+	iniciarContexto(&principal);	
 	makecontext(&gerente, gerenciador, 0); //passa a funcao "gerenciador" que gerencia as threads para a thread gerente
 	dccthread_create("principal", func, param);
+	printf("debug\n");
+	swapcontext(&principal, &gerente);
+exit(0); //exit(0) para parar de dar warning de falha de retorno
 
 	/*//verificar a funcao passada por parametro
 	principal = dccthread_create("principal", f1, 0);
@@ -39,16 +69,19 @@ void dccthread_init(void (*func)(int), int param){
 
 
 dccthread_t * dccthread_create(const char *name, void (*func)(int ), int param){
-fila[ultimo] = (dccthread_t *) malloc (sizeof(dccthread_t *));
-getcontext(fila[ultimo]->estado);
-fila[ultimo]->estado.uc_link = &gerente;
-iniciarContexto(fila[ultimo]->estado);
-makecontext(fila[ultimo]->estado, (void(*) (void)) func, 1, param);
+fila[ultimo] = (dccthread_t *) malloc(sizeof(dccthread_t *));	
 
-dccthread_t *saida = fila[ultimo];
+
+//printf("%d\n", aux->uc_sigmask); 
+getcontext(&(fila[ultimo]-> estado));
+fila[ultimo]-> estado.uc_link = &gerente; 
+iniciarContexto(&(fila[ultimo]-> estado)); 
+
+makecontext(&(fila[ultimo]-> estado), (void (*)(void)) func, 1, param);
+dccthread_t *retorno = fila[ultimo];
 ultimo ++;
 ultimo = ultimo % TAM;//limita a fila ao tamanha maximo da constante TAM
-return saida;
+return retorno;
 }
 
 
