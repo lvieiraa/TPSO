e506c7697412086452b0a574e4014d2541c01410
Leandro Vieira <lvieiraa@users.noreply.github.com>
2016-04-23 22:40:09 -0300

    implementado as funcoes  dccthread_init(void (*func)(int), int param),  e   dccthread_t * dccthread_create(const char *name, void (*func)(int ), int param)
    Falta implementar:
    dccthread_self, dccthread_name., dccthread_name e a funcao  gerenciador.

--- dccthread.c
+++ dccthread.c
@@ -3,24 +3,52 @@
 #include <pthread.h>
 #include <ucontext.h>
 #include "dccthread.h"
+#include <signal.h>
 
+#define TAM 400 //tamanho maximo da fila de threads
+#define handle_error(msg) \
+           do { perror(msg); exit(EXIT_FAILURE); } while (0) //mensagem de erro copiado do man da funcaos da biblioteca ucontext.h
+//inciar fila de prontos e fila de espera
+dccthread_t * fila[TAM];
+int atual = 0; 
+int ultimo = 0;
+
+ucontext_t gerente, principal;
+
+void iniciarContexto(ucontext_t *contexto)
+{
+  contexto->uc_stack.ss_sp = malloc(SIGSTKSZ); //SIGSTKSZ definido na biblioteca signal.h  tamanho do sinal 
+  contexto->uc_stack.ss_size = SIGSTKSZ;
+  contexto->uc_stack.ss_flags = 0;
+}
 
 
 void dccthread_init(void (*func)(int), int param){
 	
-	//inciar fila de prontos e fila de espera
 	
+	if (getcontext(&gerente) == -1) //inicia  a struct gerente 
+	handle_error("getcontext");
+	iniciarContexto(&gerente);//inicia a pilha, preenchendo  os campos necessarios da uc_stack 
+	makecontext(&gerente, gerenciador, 0); //passa a funcao "gerenciador" que gerencia as threads para a thread gerente
+	dccthread_create("principal", func, param);
 
-	//verificar a funcao passada por parametro
+	/*//verificar a funcao passada por parametro
 	principal = dccthread_create("principal", f1, 0);
-	gerente = dccthread_create("gerente", f1, 0);
+	gerente = dccthread_create("gerente", f1, 0);*/
 }
 
 
 dccthread_t * dccthread_create(const char *name, void (*func)(int ), int param){
+fila[ultimo] = (dccthread_t *) malloc (sizeof(dccthread_t *));
+getcontext(fila[ultimo]->estado);
+fila[ultimo]->estado.uc_link = &gerente;
+iniciarContexto(fila[ultimo]->estado);
+makecontext(fila[ultimo]->estado, (void(*) (void)) func, 1, param);
 
-	return 0; 
-
+dccthread_t *saida = fila[ultimo];
+ultimo ++;
+ultimo = ultimo % TAM;//limita a fila ao tamanha maximo da constante TAM
+return saida;
 }
 
 
